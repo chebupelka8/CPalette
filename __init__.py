5f9ecc7f338313d4718372f518f8b33a39094f1c
chebupelka <119750562+chebupelka8@users.noreply.github.com>
2024-01-21 16:16:18 +0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
-from color_palette.color_converter import ColorConverter
-from color_palette.colors import *
-from color_palette.preview import Previewer
+from .color_converter import *
+from .colors import *
+from .preview import *
